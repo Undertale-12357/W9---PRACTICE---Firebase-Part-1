a7f71dac232e348b82db0b256b3215476dbff07a
UndertaleXFrisk <eng.vireaksathya@gmail.com>
2026-03-19 14:11:20 +0700

    update gitignore

---
+++ lib/Term2/BlaBlaBus/lib/data/repositories/ride/ride_repository_mock.dart
@@ -0,0 +1,12 @@
+import '../../../data/dummy_data.dart';
+import '../../../model/ride/ride.dart';
+import 'ride_repository.dart';
+
+class RideRepositoryMock implements RideRepository {
+
+  @override
+  Future<List<Ride>> fetchRides() async {
+    return Future.value(fakeRides);
+  }
+
+}
